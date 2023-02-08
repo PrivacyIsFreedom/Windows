c71b5a9586fec9c6db7660f632986444f73fb81e
Privacy is Freedom <108864743+Windows-Management@users.noreply.github.com>
2023-02-08 15:38:30 +0000

    Add files via upload

---
+++ UAC admin require password.bat
@@ -0,0 +1,8 @@
+rem =================================== Windows Policies ===================================
+rem --------------------------------- User Account Control ---------------------------------
+
+rem 0 - Elevate without prompting / 1 - Prompt for credentials on the secure desktop / 2 - Prompt for consent on the secure desktop / 3 - Prompt for credentials / 4 - Prompt for consent / 5 (Default) - Prompt for consent for non-Windows binaries
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "1" /f
+
+rem 0 - Automatically deny elevation requests / 1 - Prompt for credentials on the secure desktop / 3 (Default) - Prompt for credentials
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorUser" /t REG_DWORD /d "0" /f
